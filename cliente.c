276621f687e56364b71608ad9a92b202c9af95a7
RiverCarnival <dtmnex@gmail.com>
2023-05-12 19:26:14 -0400

    Comentarios

--- cliente.c
+++ cliente.c
@@ -1,45 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <math.h>
+#include <stdio.h> // printf
+#include <stdlib.h> // exit 
+#include <string.h> // strlen
+#include <sys/socket.h> //socket
+#include <arpa/inet.h> // inet_pton AF_INET
+#include <math.h> // pow
+#include <unistd.h>// read
 #define PORT 8080
 
 
-void cliente(int sock, int valread, struct sockaddr_in serv_addr, char *buffer, char *nickname, int cript, int e, int n)
+void cliente(int sock, int valread, struct sockaddr_in serv_addr, char *buffer, int e, int n)
 {
     
+    // cria um soket TCP, devido ao sock_stream
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
     {
-        printf("\n Falha ao criar socket \n");
+        printf("\nFalha ao criar socket\n");
         return;
     }
-
+    // configura o endereço ipv4 e a porta que será ouvida
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    
+    // converte de texto o ip para o formato binario C é lindo
     if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
     {
-        printf("\nEndereço inválido/ Não suportado \n");
+        printf("\nEndereco invalido\n");
         return;
     }
 
+    //tenta conectar ao servidor, passa o soket o ponteiro da estrutura e o tamanho dela, se for menor 0 deu ruim
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
     {
-        printf("\nFalha na conexão \n");
+        printf("\nFalha na conexao \n");
         return;
     }
     
-    //send(sock, nickname, strlen(nickname), 0);
 
+    // Loop de envio de mensagens e criptografia
     while(1) 
     {
         printf("Insira a menssagem: ");
         scanf("%s", buffer);
-        
+    
+        // se a mensagem for exit, fecha o cliente, porém antes manda pro servidor para ele fechar também
      if(strcmp(buffer, "exit") == 0)
     {
         send(sock, buffer, strlen(buffer), 0);
@@ -47,7 +50,7 @@ void cliente(int sock, int valread, struct sockaddr_in serv_addr, char *buffer, 
         return;
     }
 
-
+        // o laço da criptografia com a chave publica
         for (int i = 0; i < strlen(buffer); i++) 
         {
             int num = (int)buffer[i]; 
@@ -55,27 +58,27 @@ void cliente(int sock, int valread, struct sockaddr_in serv_addr, char *buffer, 
             buffer[i] = num + cript; 
         }
 
+
+        //envia a mensagem
         send(sock, buffer, strlen(buffer), 0);
         printf("mensagem enviada\n");
 
    
-        
+        //apaga o buffer, que é a variavel da mensagem
         memset(buffer, 0, sizeof(buffer));
     }
-
 
     return;
 }
 
 int main() 
 {
-    int sock = 0, valread, cript, e = 23, n = 33;
+    int sock = 0, valread, e = 23, n = 33;
     struct sockaddr_in serv_addr;
     char buffer[1024] = {0};
-    char *nickname = "River";
 
-    cliente(sock, valread, serv_addr, buffer, nickname, cript, e, n);
+    cliente(sock, valread, serv_addr, buffer, e, n);
 
-
+    printf("\nProgramando por um mundo melhor!\n"); //sim é só eu enchendo o saco kkkk
     return 0;
 }
--- servidor.c
+++ servidor.c
@@ -1,60 +1,67 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <math.h>
-#include <unistd.h>
+#include <stdio.h> // printf
+#include <stdlib.h> // exit 
+#include <string.h> // strlen
+#include <sys/socket.h> //socket
+#include <arpa/inet.h> // inet_pton AF_INET
+#include <math.h> // pow
+#include <unistd.h>// read
 #define PORT 8080
 
-
+// função principal que cria o servidor
 void servidor(int server_fd, int new_socket, int valread, int opt, struct sockaddr_in address, int addrlen, char *message, char *buffer, int d, int n)
 {
-   
+   // aqui é criado o servidor TCP, se o retorno foi diferente de 0, quer dizer que deu tudo errado na criação do soket
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
-        perror("Falha ao criar socket");
+        printf("Falha ao criar socket");
         exit(EXIT_FAILURE);
     }
 
+    // Aqui se o servidor for desligado abrupdamente ele libera o acesso ao endereço e a porta, não deixando ocupado caso feche o server
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) 
     {
-        perror("setsockopt");
+        printf("Desligando o server, fechamento inesperado");
         exit(EXIT_FAILURE);
     }
 
+    //Atribuição do IP e porta e indicação de quais portas ele deve ouvir
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-
+    //atribuir endereço e porta, se for menor que 0 significa que deu tudo errado e o server não foi criado
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
     {
-        perror("Falha ao atribuir endereço ao socket");
+        printf("Falha ao atribuir endereço ao socket");
         exit(EXIT_FAILURE);
     }
 
-
+    // quantas conexões ele pode aceitar, se for menor que 0 algo deu errado
     if(listen(server_fd, 3) < 0)
     {
-        perror("Falha ao ouvir por conexões");
+        printf("Falha ao ouvir por conexões");
         exit(EXIT_FAILURE);
     }
 
-
+    // aceita novas entradas na rede
     if((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0)
     {
-        perror("Falha ao aceitar conexão");
+        printf("Falha ao aceitar conexão");
         exit(EXIT_FAILURE);
     }
     
+
+    // envia uma mensagem para o cliente
     send(new_socket, message, strlen(message), 0);
 
 
+    //loop de funcionamento do servidor para escutar o que o cleinte envia
     while(1)
     {
+        // lê o que chegou no soket
         valread = read(new_socket, buffer, 1024);
         
+        // descriptografa a mensagem do cliente usando a chave privada
         for (int i = 0; i < strlen(buffer); i++) 
         {
             int num = (int) buffer[i];
@@ -62,16 +69,20 @@ void servidor(int server_fd, int new_socket, int valread, int opt, struct sockad
             buffer[i] = num - decript; 
         }
 
+        // exibe o que o cliente enviou 
         printf("Usuario: %s\n", buffer);
 
+        // se for um EXIT, o soket fecha junto com o programa
         if (strcmp(buffer, "exit") == 0)
         {
             exit(EXIT_SUCCESS);
             return;
         }
 
+        //Envia a mesma mensagem que chegou para os clientes
         send(new_socket, buffer, strlen(buffer), 0);
 
+        //limpa o buffer
         memset(buffer, 0, sizeof(buffer));
     }
 
@@ -88,5 +99,7 @@ int main() {
 
     servidor(server_fd, new_socket, valread, opt, address, addrlen, message, buffer, d, n);
 
+
+    printf("\nProgramando por um mundo melhor!\n"); //sim é só eu enchendo o saco kkkk
     return 0;
 }
